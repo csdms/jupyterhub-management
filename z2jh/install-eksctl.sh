ee0b2494ba4a6ee395a24d8445e5616aa8384000
Mark Piper <mark.piper@colorado.edu>
2024-06-21 15:37:26 -0600

    Embiggen if block to define install prefix

--- z2jh/install-eksctl.sh
+++ z2jh/install-eksctl.sh
@@ -1,4 +1,11 @@
 #! /usr/bin/env bash
+# Install `eksctl`, the AWS EKS CLI.
+
+if [[ -z "${CONDA_PREFIX}" ]]; then
+    prefix="/usr/local"
+else
+    prefix="${CONDA_PREFIX}"
+fi
 
 # for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
 # ARCH=amd64
@@ -12,5 +19,4 @@ curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_ch
 
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
-[[ -z "${CONDA_PREFIX}" ]] && prefix='/usr/local' || prefix="${CONDA_PREFIX}"
 sudo mv /tmp/eksctl $prefix/bin
