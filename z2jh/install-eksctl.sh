4fbdabd924faea604784705f880bd5ae2009f920
Mark Piper <mark.piper@colorado.edu>
2024-06-22 18:05:15 -0600

    Use shasum instead of sha256sum

--- z2jh/install-eksctl.sh
+++ z2jh/install-eksctl.sh
@@ -19,7 +19,7 @@ PLATFORM=${OS}_${ARCH}
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 
 # (Optional) Verify checksum
-curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | shasum --check
 
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 
